109d28cceea07ac0810a6f2e25316b5ea84853f5
Prakash L <121917226+prakashlohanathan@users.noreply.github.com>
2023-06-21 13:29:54 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+
+let OddNo = (array) => {
+    for(let value of array){
+               if(value % 2 != 0){
+                   console.log(value);
+   } }} 
+     (OddNo([1,2,3,4,5,6,7,8,9,10]));
+     
